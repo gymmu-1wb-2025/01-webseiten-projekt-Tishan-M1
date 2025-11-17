24b8474dc253145b1b7b43d96bb0310f17135cf0
Tishan Murugesan <e237536@sbl.edu.ch>
2025-11-17 09:24:00 +0000

    Main.js erstellt

---
+++ main.js
@@ -0,0 +1,11 @@
+console.log("Hello World");
+
+let counter = 0;
+function createNewElement() {
+  let newElement = document.createElement("p");
+
+  counter = counter + 1;
+  newElement.textContent = "Element " + counter;
+  let mainElement = document.querySelector("main");
+  mainElement.appendChild(newElement);
+}
