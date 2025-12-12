d54da054103dad94a3a1152b415daf93905546bd
Tishan Murugesan <e237536@sbl.edu.ch>
2025-12-12 12:34:43 +0000

    Java

---
+++ main.js
@@ -0,0 +1,10 @@
+console.log("Meine Webseite");
+let counter = 0;
+function createNewElement() {
+  letNewElement = document.createElement("h1");
+
+  counter = counter + 1;
+  newElement.textContent = "Element" + "counter";
+  let mainElement = document.querySelector("header");
+  titleElement.textContent;
+}
