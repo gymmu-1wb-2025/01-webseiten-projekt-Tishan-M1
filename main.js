abe33c1a77b5f6b36cfc1ea12153363e8cd23d49
Tishan Murugesan <e237536@sbl.edu.ch>
2025-12-17 19:52:34 +0000

    WIP: Farben auf der Seite anpassen

--- main.js
+++ main.js
@@ -15,3 +15,7 @@ document.addEventListener("keydown", increaseCount);
 titleElement.addEventListener("click", increaseCount);
 
 setInterval(increaseCount, 1000);
+
+function changeColor(color) {
+  document.documentElement.style.setProperty("--section-color", color);
+}
